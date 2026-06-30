8147ce69760f73ebf326e67ff52898c18515b8ef
Keerthanags01 <keerthanasathish321@gmail.com>
2026-06-30 14:25:41 +0530

    Completed JUnit Exercise 1

--- TDD using Junit and mockito/Junit exercises/Exercise1/Exercise1/src/test/java/com/cognizant/junit/CalculatorTest.java
+++ TDD using Junit and mockito/Junit exercises/Exercise1/Exercise1/src/test/java/com/cognizant/junit/CalculatorTest.java
@@ -1,11 +1,36 @@
 package com.cognizant.junit;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
     @Test
-    public void testAddition() {
-        System.out.println("JUnit Setup Successful!");
+    public void testAdd() {
+        assertEquals(15, calculator.add(10, 5));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(5, calculator.subtract(10, 5));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(50, calculator.multiply(10, 5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(10, 5));
     }
 }
