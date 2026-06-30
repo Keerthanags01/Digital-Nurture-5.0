21478c5c49145ab2e096d2a07ad6d38f4504992f
Keerthanags01 <keerthanasathish321@gmail.com>
2026-06-30 13:32:31 +0530

    Completed JUnit Exercise 1 - Setting Up JUnit

---
+++ TDD using Junit and mockito/Junit exercises/Exercise1/Exercise1/src/test/java/com/cognizant/junit/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.cognizant.junit;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        System.out.println("JUnit Setup Successful!");
+    }
+}
