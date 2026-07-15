df8687df14f3055397a0dc2599a96663fe21882a
Keerthana Sathish <keerthanasathish321@gmail.com>
2026-07-15 21:57:40 +0530

    Completed Angular Hands-On 4 - Template-driven forms and validation

--- Angular/student-course-portal/src/app/components/header/header.ts
+++ Angular/student-course-portal/src/app/components/header/header.ts
@@ -3,8 +3,11 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header { }
+export class Header {
+
+}
