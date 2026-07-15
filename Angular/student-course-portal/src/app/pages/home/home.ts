0e0ade4e381b7fd9e268014e0225f8f414ef36a1
Keerthana Sathish <keerthanasathish321@gmail.com>
2026-07-15 21:00:46 +0530

    Completed Angular Hands-On 2 - Data binding, lifecycle hooks and component communication

--- Angular/student-course-portal/src/app/pages/home/home.ts
+++ Angular/student-course-portal/src/app/pages/home/home.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit, OnDestroy {
+
+  // Interpolation
+  portalName = 'Student Course Portal';
+
+  // Property Binding
+  isPortalActive = true;
+
+  // Event Binding
+  message = '';
+
+  // Two-Way Binding
+  searchTerm = '';
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+
+  /*
+    [property] is one-way binding (Component → DOM)
+
+    [(ngModel)] is two-way binding (Component ↔ DOM)
+
+    Changes in either the component or the input field
+    automatically update the other.
+  */
+
+  // Lifecycle Hook - called once after component is initialized
+  ngOnInit(): void {
+    console.log('HomeComponent initialized - courses loaded');
+  }
+
+  // Lifecycle Hook - called just before component is destroyed
+  ngOnDestroy(): void {
+    console.log('HomeComponent destroyed');
+  }
 
 }
